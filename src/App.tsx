7086e21b19dcbb91b50554e23559b92473542b37
max329prex-bit <max329prex@gmail.com>
2026-06-11 05:34:35 +0100

    feat: add /app/admin/margin route for ProviderMarginReport

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ import TreasuryDashboard from "./pages/app/admin/TreasuryDashboard.tsx";
 import SupportCenter from "./pages/app/admin/SupportCenter.tsx";
 import FraudMonitor from "./pages/app/admin/FraudMonitor.tsx";
 import Broadcast from "./pages/app/admin/Broadcast.tsx";
+import ProviderMarginReport from "./pages/app/admin/ProviderMarginReport.tsx";
 import { useAdminRole } from "./hooks/useAdminRole.tsx";
 import { BoltLoader } from "./components/swift/BoltLoader.tsx";
 
@@ -84,6 +85,7 @@ const App = () => (
               <Route path="admin/support" element={<RequireAdmin><SupportCenter /></RequireAdmin>} />
               <Route path="admin/fraud" element={<RequireAdmin><FraudMonitor /></RequireAdmin>} />
               <Route path="admin/broadcast" element={<RequireAdmin><Broadcast /></RequireAdmin>} />
+              <Route path="admin/margin" element={<RequireAdmin><ProviderMarginReport /></RequireAdmin>} />
             </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
